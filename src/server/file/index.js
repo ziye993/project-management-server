798c2b2cd2252ba9b30ffb7aca90d87747e04b52
ziye993 <2996447644@qq.com>
2025-11-09 23:18:28 +0800

    创建file模块

---
+++ src/server/file/index.js
@@ -0,0 +1,8 @@
+import app from '../../app.js';
+
+let cachFileList = [];
+
+
+app.get("/api/file/getList", (req, res) => {
+
+})
---
+++ src/server/user/index.js
@@ -0,0 +1,5 @@
+import app from '../../app.js';
+
+app.post('/api/user/login', (req, res) => {
+
+});
